3eb9bb643d4a3ff20d96c31c1da9bafdb8c7edef
RossEvv <ros22ch.caceres@gmail.com>
2020-04-17 23:36:22 -0500

    bolierplate

---
+++ test/mdlinks.spec.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
+import suma from '../src/index.js';
+
+test('sumar 1 + 2 es igual a 3', () => {
+  expect(suma(1, 2)).toBe(3);
+});
