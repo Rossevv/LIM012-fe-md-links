4838f01ef42ecdd193288e868ba3fb6b537cfb98
RossEvv <ros22ch.caceres@gmail.com>
2020-04-19 09:54:27 -0500

    bolierplate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-function suma(a, b) {
-  return a + b;
+/* eslint-disable no-console */
+// function suma(a, b) {
+//   return a + b;
+// }
+// export default suma;
+const prueba = './some/example.md';
+
+if (fs.existsSync(prueba)) {
+  console.log('The path exists.');
 }
-export default suma;
